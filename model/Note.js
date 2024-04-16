947f3c6e1463358db770934d8693be8b25b4bafd
zouyos <hamza.benketaf@gmail.com>
2024-04-16 08:44:45 +0200

    'modified_at' added

--- model/Note.js
+++ model/Note.js
@@ -4,6 +4,7 @@ const noteSchema = mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   created_at: { type: String, required: true },
+  modified_at: { type: String },
 });
 
 module.exports = mongoose.model("Note", noteSchema);
