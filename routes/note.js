ef72ad7801422c1992c8b56599f2e30165707485
zouyos <hamza.benketaf@gmail.com>
2024-04-19 17:24:11 +0200

    auth part 5

--- routes/note.js
+++ routes/note.js
@@ -4,10 +4,33 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const noteCtrl = require("../controller/note");
 
-router.post("/", auth, noteCtrl.create);
+const { body } = require("express-validator");
+const validate = require("../middleware/validation");
+
+const validationRules = [
+  body("title")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Title must be defined")
+    .isLength({ min: 3 })
+    .withMessage("Min 3 characters")
+    .isLength({ max: 50 })
+    .withMessage("Max 50 characters"),
+  body("content")
+    .trim()
+    .escape()
+    .notEmpty()
+    .withMessage("Content must be defined")
+    .isLength({ min: 3 })
+    .withMessage("Min 3 characters"),
+  validate,
+];
+
+router.post("/", validationRules, auth, noteCtrl.create);
 router.get("/", auth, noteCtrl.getAll);
 router.get("/:id", auth, noteCtrl.getById);
-router.patch("/:id", auth, noteCtrl.updateById);
+router.patch("/:id", validationRules, auth, noteCtrl.updateById);
 router.delete("/:id", auth, noteCtrl.deleteById);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,25 @@ const router = express.Router();
 
 const userCtrl = require("../controller/user");
 
-router.post("/signup", userCtrl.signup);
-router.post("/login", userCtrl.login);
+const { body } = require("express-validator");
+const validate = require("../middleware/validation");
+
+const validationRules = [
+  body("email")
+    .isEmail()
+    .normalizeEmail()
+    .withMessage("Incorrect email format"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password too short (min 6 characters)")
+    .isLength({ max: 30 })
+    .withMessage("Password too long (max 30 characters)")
+    .matches(/^(?=.*[A-Z])[A-Za-z0-9]+$/)
+    .withMessage("Only letters (at least one uppercase) and numbers allowed"),
+  validate,
+];
+
+router.post("/signup", validationRules, userCtrl.signup);
+router.post("/login", validationRules, userCtrl.login);
 
 module.exports = router;
