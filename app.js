01500ace4d892670ac6d7227a6d160125012b1c5
zouyos <hamza.benketaf@gmail.com>
2024-04-20 15:37:55 +0200

    .env add front end domain

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", process.env.FRONT_END_DOMAIN);
   res.setHeader(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
