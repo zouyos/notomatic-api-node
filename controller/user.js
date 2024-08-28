5c4387abce1d05be645ab0b6171b06e24bdbc7ef
zouyos <hamza.benketaf@gmail.com>
2024-08-28 19:11:13 +0200

    email formatting

--- controller/user.js
+++ controller/user.js
@@ -88,9 +88,9 @@ exports.requestPasswordReset = (req, res, next) => {
         from: process.env.EMAIL_USER,
         subject: 'Password Reset',
         text: `You are receiving this because you have requested the reset of the password for your account.\n\n
-               Please click on the following link, or paste this into your browser to complete the process:\n\n
-               https://${process.env.FRONT_END_DOMAIN}/reset/${token}\n\n
-               If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+        Please click on the following link, or paste this into your browser to complete the process:\n\n
+        https://${process.env.FRONT_END_DOMAIN}/reset/${token}\n\n
+        If you did not request this, please ignore this email and your password will remain unchanged.\n`,
       };
 
       return transporter.sendMail(mailOptions);
