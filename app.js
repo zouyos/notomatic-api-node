57776484275119aae5905250d499a5a44a11c1ab
zouyos <hamza.benketaf@gmail.com>
2024-11-03 13:46:52 +0100

    app.options

--- app.js
+++ app.js
@@ -16,10 +16,6 @@ const app = express();
 
 app.use(express.json());
 
-app.options('*', (req, res) => {
-  res.sendStatus(200);
-});
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', process.env.FRONT_END_DOMAIN);
   res.setHeader(
@@ -31,6 +27,21 @@ app.use((req, res, next) => {
     'GET, POST, PUT, DELETE, PATCH, OPTIONS'
   );
   res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
+app.options('*', (req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', process.env.FRONT_END_DOMAIN);
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.sendStatus(200);
   next();
 });
 
